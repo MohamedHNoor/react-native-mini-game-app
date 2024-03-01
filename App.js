c96b8c971a5e49762abddff11518436ed48fece2
Mohamed Hassan Noor <mohmed.hassannoor114@gmail.com>
2024-03-01 18:25:39 +0200

    created expo react-native app with tailwindcss

--- App.js
+++ App.js
@@ -3,18 +3,9 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View className='flex-1 items-center justify-center bg-blue-200'>
+      <Text className='text-red-500'>Hello world</Text>
+      <StatusBar style='auto' />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
