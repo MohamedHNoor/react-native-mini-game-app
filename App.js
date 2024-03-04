6385aca71dc56622879f27a9986b32d0993055d2
Mohamed Hassan Noor <mohmed.hassannoor114@gmail.com>
2024-03-04 13:37:38 +0200

    add save screen and route option

--- App.js
+++ App.js
@@ -1,14 +1,28 @@
-import { ImageBackground, Text } from 'react-native';
+import { useState } from 'react';
+import { ImageBackground, SafeAreaView } from 'react-native';
 import StartGameScreen from './screens/StartGameScreen';
+import GameScreen from './screens/GameScreen';
 
 export default function App() {
+  const [userNumber, setUserNumber] = useState();
+
+  const PickedNumberHandler = (number) => {
+    setUserNumber(number);
+  };
+
+  let screen = <StartGameScreen onPickNumber={PickedNumberHandler} />;
+
+  if (userNumber) {
+    screen = <GameScreen />;
+  }
+
   return (
     <ImageBackground
       source={require('./assets/images/background.png')}
       resizeMode='cover'
       className='flex-1 opacity-100'
     >
-      <StartGameScreen />
+      <SafeAreaView className='flex-1 opacity-100'>{screen}</SafeAreaView>
     </ImageBackground>
   );
 }
